1671bbbd956f62e2f3aad634d00526bd0be5a360
Prathamesh Nimkar <zekhram007@gmail.com>
2025-03-04 17:15:50 +0530

    changed edu details ; added images

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,11 +1,16 @@
 import BounceCards from "./animations/bounceCards";
+import pahili from "../../public/1st.png";
+import shala from "../../public/ssrvm.jpeg";
+import baravi from "../../public/12th.jpeg";
+import iitpk from "../../public/iitpk.jpg"
+import uni from "../../public/MUJ.webp";
 
 const images = [
-    "https://picsum.photos/400/400?grayscale",
-    "https://picsum.photos/500/500?grayscale",
-    "https://picsum.photos/600/600?grayscale",
-    "https://picsum.photos/700/700?grayscale",
-    "https://picsum.photos/300/300?grayscale"
+    {uni},
+    {baravi},
+    {iitpk},
+    {shala},
+    {pahili}
 ];
 
 const transformStyles = [
@@ -17,11 +22,11 @@ const transformStyles = [
 ];
 
 const educationDetails = [
-    "High School: 2015-2018, GPA: 3.9, Science Club Leader",
-    "Bachelor's Degree: 2018-2022, GPA: 3.8, Dean's List",
-    "Master's Degree: 2022-2024, Research in AI, Published Paper",
-    "Certification: 2024, Full-Stack Development, Merit",
-    "Workshop: 2023, UX Design & Accessibility, Certification"
+    "B. Tech. in Information Technology: Aug. 2021 - Jul. 2025, GPA: 8.81, Dean's List in 4th, 5th, 6th, 7th Semesters",
+    "12th Grade : Maharashtra State Board: Jun. 2020 - Apr. 2021, Grade: 92%, ",
+    "Foundation classes: Sept. 2019 - May 2021, Appeared for JEE Mains, cleared and appeared for JEE Advanced, MHT-CET",
+    "Secondary School: CBSE (Central Board for Secondary Education): Jul. 2009 - Apr. 2019, Grade: 95%, Was a part of the school's football, basketball teams, Also had Vocal Music as a special subject and was a part of the school's music club, performing in 5 classical music shows, was a class representative in my 10th Grade.",
+    "Primary School: 1st Grade, 2008 - 2009"
 ];
 
 export default function Education() {
