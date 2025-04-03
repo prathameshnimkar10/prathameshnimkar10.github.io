a2ccc918589e1f13bab210144df6606eb70c1b89
Prathamesh Nimkar <zekhram007@gmail.com>
2025-04-03 14:49:26 +0530

    built, changes made for deployment to pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  reactStrictMode: true,
+
+  basePath: "/prathameshnimkar10.github.io",
+  assetPrefix: "/prathameshnimkar10.github.io/"
 };
 
 export default nextConfig;
